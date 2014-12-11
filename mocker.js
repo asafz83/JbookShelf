035d1ca622832380d0c61b879b5755619068e43b
asafz83 <asafz83@gmail.com>
2014-12-11 20:53:51 +0200

    Create mocker.js

---
+++ mocker.js
@@ -0,0 +1,25 @@
+$.mockjax({
+			  url: "/books",
+			  response: function(){
+			    if (localStorage.getItem("refael") == null || localStorage.getItem("refael").length == 0) {
+          	localStorage.setItem("refael", "[]"); 	
+          	this.responseText = {"data": []};
+          	} else {
+          		var a = localStorage.getItem("refael");
+          		booksArray = JSON.parse(a);
+            	this.responseText = {"data": booksArray};	
+          	}
+			  }
+			});
+			
+			$.mockjax({
+			  url: "/books/new",
+			  response: function(settings) {
+		      var book = settings.data;
+		      var a = localStorage.getItem("refael");
+		      booksArray = JSON.parse(a);
+		      booksArray.push(book);
+		      localStorage.setItem("refael", JSON.stringify(booksArray));
+		      this.responseText = {"status": "200"};
+				}
+			});
